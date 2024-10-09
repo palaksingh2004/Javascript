ba3c796267a536d9883aaee32625cbe4bf8cdeca
palaksingh2004 <142495401+palaksingh2004@users.noreply.github.com>
2024-10-09 12:50:29 +0000

    congfigure message

--- variables.js
+++ variables.js
@@ -2,5 +2,11 @@ const accountId =  144675
 let accountEmail = "palaksingh.com"
 var accountPassword = "12345"
 accountCity = "bhilai"
+let accountState;
 
 accountEmail = "shivamsingh234"
+var accountPassword = "56865724"
+accountCity = "bihar"
+
+
+console.table([accountEmail, accountId, accountPassword,accountCity, accountState]);
